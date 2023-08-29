9344d36bbb6e278df40ff90d918ed9ea16b26b96
PedroCruz <contatocreed67@gmail.com>
2023-08-29 15:17:04 -0400

    paginations adding

--- src/routes/autorRoutes.js
+++ src/routes/autorRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import AutorController from "../controller/autoresController.js";
+import pagination from "../middlewares/paginations.js";
 
 export const autorRouter = express.Router();
 
 autorRouter
-  .get("", AutorController.listarAutores)
+  .get("", AutorController.listarAutores, pagination)
   .get("/:id", AutorController.listarAutorPorId)
   .post("", AutorController.cadastrarAutor)
   .put("/:id", AutorController.atualizarAutor)
--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import LivroController from "../controller/livrosController.js";
+import pagination from "../middlewares/paginations.js";
 
 export const livroRouter = express.Router();
 
 livroRouter
-  .get("", LivroController.listarLivros)
-  .get("/busca", LivroController.listarLivrosPorFiltro)
+  .get("", LivroController.listarLivros, pagination)
+  .get("/busca", LivroController.listarLivrosPorFiltro, pagination)
   .get("/:id", LivroController.listarLivrosPorId)
   .post("", LivroController.cadastrarLivros)
   .put("/:id", LivroController.atualizarLivros)
